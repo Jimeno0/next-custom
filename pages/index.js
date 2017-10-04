7a5a0716f6f25a5d045b4733a3cf10cc440421e4
Jimeno0 <c.p.jimeno0@gmail.com>
2017-10-04 09:56:25 +0200

    feat(With a Link tag):

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import { Router, Link, getRoute } from '../routes';
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `;
 
-export default () => <Title>My page</Title>;
+
+class IndexRoute extends Component {
+  render() {
+    return (
+      <section className={this.props.className}>
+        <Title>My page</Title>
+        <Link route={getRoute('user', { user: 123 })}>
+          <a>Go to users</a>
+        </Link>
+      </section>
+    );
+  }
+}
+
+export default IndexRoute;
+
